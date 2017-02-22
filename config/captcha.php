93a19b4a20307b1a6282bf389d865a93483d1adf
keleshteri <mehdi.shaban.keleshteri@gmail.com>
2017-02-22 18:04:57 +0800

    add config

---
+++ config/captcha.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    /**
+     * API Keys
+     **/
+    'secret'  => env('NOCAPTCHA_SECRET'),
+    'sitekey' => env('NOCAPTCHA_SITEKEY'),
+   /**
+     * Version
+     **/
+
+    'version'     => 2,
+];
